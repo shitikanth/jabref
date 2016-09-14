be76084cd99690126f7af3d8ac22e86041104b1f
tschechlovdev <tschechlov.dev@gmail.com>
2016-09-14 14:51:50 +0200

    Revert "Add search test"

--- src/test/java/net/sf/jabref/logic/search/DatabaseSearcherTest.java
+++ src/test/java/net/sf/jabref/logic/search/DatabaseSearcherTest.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import net.sf.jabref.model.database.BibDatabase;
 import net.sf.jabref.model.entry.BibEntry;
-import net.sf.jabref.model.entry.BibtexEntryTypes;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,35 +14,32 @@ public class DatabaseSearcherTest {
 
     public static final SearchQuery INVALID_SEARCH_QUERY = new SearchQuery("\\asd123{}asdf", true, true);
 
-    private BibDatabase database;
-
-
-    @Before
-    public void setUp() {
-        database = new BibDatabase();
-    }
 
     @Test
-    public void testNoMatchesFromEmptyDatabase() {
+    public void testGetDatabaseFromMatchesEmptyDatabase() {
+        BibDatabase database = new BibDatabase();
         List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("whatever", true, true), database).getMatches();
         assertEquals(Collections.emptyList(), matches);
     }
 
     @Test
-    public void testNoMatchesFromEmptyDatabaseWithInvalidSearchExpression() {
+    public void testGetDatabaseFromMatchesEmptyDatabaseInvalidSearchExpression() {
+        BibDatabase database = new BibDatabase();
         List<BibEntry> matches = new DatabaseSearcher(INVALID_SEARCH_QUERY, database).getMatches();
         assertEquals(Collections.emptyList(), matches);
     }
 
     @Test
-    public void testNoMatchesFromDatabaseWithEmptyEntry() {
+    public void testGetDatabaseFromMatchesDatabaseWithEmptyEntries() {
+        BibDatabase database = new BibDatabase();
         database.insertEntryWithDuplicationCheck(new BibEntry());
         List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("whatever", true, true), database).getMatches();
         assertEquals(Collections.emptyList(), matches);
     }
 
     @Test
-    public void testNoMatchesFromDatabaseWithArticleTypeEntry() {
+    public void testGetDatabaseFromMatchesDatabaseWithEntries() {
+        BibDatabase database = new BibDatabase();
         BibEntry entry = new BibEntry();
         entry.setType("article");
         entry.setField("author", "harrer");
@@ -54,74 +49,13 @@ public class DatabaseSearcherTest {
     }
 
     @Test
-    public void testCorrectMatchFromDatabaseWithArticleTypeEntry() {
+    public void testGetDatabaseFromMatchesDatabaseWithEntriesWithCorrectMatch() {
+        BibDatabase database = new BibDatabase();
         BibEntry entry = new BibEntry();
         entry.setType("article");
         entry.setField("author", "harrer");
         database.insertEntryWithDuplicationCheck(entry);
         List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("harrer", true, true), database).getMatches();
         assertEquals(Collections.singletonList(entry), matches);
-    }
-
-    @Test
-    public void testNoMatchesFromEmptyDatabaseWithInvalidQuery() {
-        SearchQuery query = new SearchQuery("asdf[", true, true);
-
-        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
-
-        assertEquals(Collections.emptyList(), databaseSearcher.getMatches());
-    }
-
-    @Test
-    public void testCorrectMatchFromDatabaseWithIncollectionTypeEntry() {
-        BibEntry entry = new BibEntry();
-        entry.setType(BibtexEntryTypes.INCOLLECTION);
-        entry.setField("author", "tonho");
-        database.insertEntry(entry);
-
-        SearchQuery query = new SearchQuery("tonho", true, true);
-        List<BibEntry> matches = new DatabaseSearcher(query, database).getMatches();
-
-        assertEquals(Collections.singletonList(entry), matches);
-    }
-
-    @Test
-    public void testNoMatchesFromDatabaseWithTwoEntries() {
-        BibEntry entry = new BibEntry();
-        database.insertEntry(entry);
-
-        entry = new BibEntry();
-        entry.setType(BibtexEntryTypes.INCOLLECTION);
-        entry.setField("author", "tonho");
-        database.insertEntry(entry);
-
-        SearchQuery query = new SearchQuery("tonho", true, true);
-        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
-
-        assertEquals(Collections.emptyList(), databaseSearcher.getMatches());
-    }
-
-    @Test
-    public void testNoMatchesFromDabaseWithIncollectionTypeEntry() {
-        BibEntry entry = new BibEntry();
-        entry.setType(BibtexEntryTypes.INCOLLECTION);
-        entry.setField("author", "tonho");
-        database.insertEntry(entry);
-
-        SearchQuery query = new SearchQuery("asdf", true, true);
-        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
-
-        assertEquals(Collections.emptyList(), databaseSearcher.getMatches());
-    }
-
-    @Test
-    public void testNoMatchFromDatabaseWithEmptyEntry() {
-        BibEntry entry = new BibEntry();
-        database.insertEntry(entry);
-
-        SearchQuery query = new SearchQuery("tonho", true, true);
-        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
-
-        assertEquals(Collections.emptyList(), databaseSearcher.getMatches());
     }
 }
